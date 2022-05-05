e4b3fa97b1e882e1c3adc7bdf12703192bb3c3ca
Bryan <bryanmontero81@gmail.com>
2022-05-04 20:55:07 -0400

    Authentication service completed

--- src/db/connectionString.js
+++
@@ -1,5 +0,0 @@
-
-
-const CONNECTIONSTRING = "mongodb+srv://bflorentino:bflorentinoAppMongoDev2302@cluster-gliskup.o0ovc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-module.exports = CONNECTIONSTRING;
