e217cad7aa137c416a28eac6e62a0364edce0069
Bryan <bryanmontero81@gmail.com>
2022-08-30 22:34:29 -0400

    Changes in environment variables

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== "Production") {
+if (process.env.NODE_ENV !== "production") {
   const envFound = dotenv.config();
   if (envFound.error) {
     throw new Error("⚠️  Couldn't find .env file");
